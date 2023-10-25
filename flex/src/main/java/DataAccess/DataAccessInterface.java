8f3ae2c73948a502e23ec4dcf37170e28bdb1a69
vithushon <v.thayalan@mail.utoronto.ca>
2023-10-25 13:17:03 -0400

    first commit to vithu branch. Created DAI

---
+++ flex/src/main/java/DataAccess/DataAccessInterface.java
@@ -0,0 +1,9 @@
+package DataAccess;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DataAccessInterface extends MongoRepository {
+}
+
+// still working on this
+
