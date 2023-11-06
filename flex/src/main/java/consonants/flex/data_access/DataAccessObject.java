4a60051a3ae34e3121eb4588d2f44d613c57f28d
vithushon <v.thayalan@mail.utoronto.ca>
2023-11-06 18:36:23 -0500

    added createClient and createClaim methods to DAO

--- flex/src/main/java/consonants/flex/data_access/DataAccessObject.java
+++ flex/src/main/java/consonants/flex/data_access/DataAccessObject.java
@@ -4,6 +4,11 @@ import consonants.flex.entity.Client;
 import consonants.flex.entity.Claim;
 import consonants.flex.entity.Form;
 import consonants.flex.use_case.create_new_claim.CreateNewClaimDataAccessInterface;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,6 +17,7 @@ public class DataAccessObject implements CreateNewClaimDataAccessInterface {
     private ClientDataAccessInterface clientDataAccessObject;
     private ClaimDataAccessInterface claimDataAccessObject;
     private FormDataAccessInterface formDataAccessObject;
+    private MongoTemplate mongoTemplate;
 
     public List<Client> allClients() {return clientDataAccessObject.findAll();}
     public List<Claim> allClaims() {return claimDataAccessObject.findAll();}
@@ -23,5 +29,21 @@ public class DataAccessObject implements CreateNewClaimDataAccessInterface {
             return true;
         }
         return false;
+    }
+
+    public Claim createClaim(ArrayList<Form> forms, int status, int clientId, int claimId) {
+        Claim claim = claimDataAccessObject.insert(new Claim(forms, status, clientId, claimId));
+
+        mongoTemplate.update(Client.class)
+                .matching(Criteria.where("clientId").is(clientId))
+                .apply(new Update().push("claimsList").value(claim))
+                .first();
+
+        return claim;
+    }
+
+    public Client createClient(int clientId, String firstName, String lastName) {
+        Client client = clientDataAccessObject.insert(new Client(clientId, firstName, lastName));
+        return client;
     }
 }
