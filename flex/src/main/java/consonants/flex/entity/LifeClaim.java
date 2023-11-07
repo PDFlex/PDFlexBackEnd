43d2cb3f5650413361ef3292b4dc2ee9b7d01547
Jiya Patel <128255337+jiyapatel4@users.noreply.github.com>
2023-11-06 21:52:23 -0500

    Delete flex/src/main/java/consonants/flex/entity/LifeClaim.java

--- flex/src/main/java/consonants/flex/entity/LifeClaim.java
+++
@@ -1,9 +0,0 @@
-package consonants.flex.entity;
-
-import java.util.ArrayList;
-
-public class LifeClaim extends Claim{
-    public LifeClaim(ArrayList<Form> forms, int status, int clientId, int claimId) {
-        super(forms, status, clientId, claimId);
-    }
-}
