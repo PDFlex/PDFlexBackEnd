03e32192fd5949e81093a1a349f893dc0605f145
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2023-11-06 23:25:12 -0500

    tried to update attributes again

--- flex/src/main/java/consonants/flex/entity/Form.java
+++ flex/src/main/java/consonants/flex/entity/Form.java
@@ -1,18 +1,40 @@
 package consonants.flex.entity;
 
-public abstract class Form {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "forms")
+@Data
+@AllArgsConstructor
+public class Form {
 
     private boolean confirmed;
     private boolean filledOut;
     private boolean inProgress;
+    private int formId;
+    private int clientId;
+    // fields in common across all three forms
+    private int claimId;
+    private String deceasedName;
+    private String dateOfDeath;
+    private String dateSigned;
 
 
-    protected Form(boolean confirmed, boolean filledOut, boolean inProgress) {
-        this.confirmed = confirmed;
-        this.filledOut = filledOut;
-        this.inProgress = inProgress;
+
+
+    public Form(int formId, int claimId, int clientId, String deceasedName, String dateOfDeath, String dateSigned) {
+        this.confirmed = false;
+        this.filledOut = false;
+        this.inProgress = true;
+        this.formId = formId; // randomly generate & will need to check that formId doesn't already exist when implementing FormUseCase - discuss further
+        this.claimId = claimId;
+        this.clientId = clientId;
+        this.deceasedName = deceasedName;
+        this.dateOfDeath = dateOfDeath;
+        this.dateSigned = dateSigned;
     }
 
     // Consider checkConfirmed(), checkFilledOut() and checkInProgress() to be
-    // the getters of their corresponding attributes
+    // the getters of their corresponding attributes.
 }
