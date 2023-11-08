3e1d193a338d93716c12be054522c3bed7b220ca
vithushon <v.thayalan@mail.utoronto.ca>
2023-11-08 17:56:30 -0500

    Changed type of attribute from ViewAllClaimsInteractor to ViewAllClaimsInputBoundary in ViewAllClaimsController

--- flex/src/main/java/consonants/flex/interface_adapter/view_all_claims/ViewAllClaimsController.java
+++ flex/src/main/java/consonants/flex/interface_adapter/view_all_claims/ViewAllClaimsController.java
@@ -3,10 +3,7 @@ package consonants.flex.interface_adapter.view_all_claims;
 
 import consonants.flex.entity.Claim;
 
-import consonants.flex.use_case.view_all_claims.ViewAllClaimsDataAccessInterface;
-import consonants.flex.use_case.view_all_claims.ViewAllClaimsInteractor;
-import consonants.flex.use_case.view_all_claims.ViewAllClaimsOutputBoundary;
-import consonants.flex.use_case.view_all_claims.ViewAllClaimsOutputData;
+import consonants.flex.use_case.view_all_claims.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +16,7 @@ import java.util.List;
 @RequestMapping("/claims")
 public class ViewAllClaimsController {
     @Autowired
-    private ViewAllClaimsInteractor viewAllClaimsInteractor;
+    private ViewAllClaimsInputBoundary viewAllClaimsInteractor;
 
     @GetMapping
     public ResponseEntity<List<Claim>> getAllClaims() {
