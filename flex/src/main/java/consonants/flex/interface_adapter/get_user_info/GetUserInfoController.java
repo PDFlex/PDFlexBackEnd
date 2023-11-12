22d13b1d92fd9b7473686e624d9273590ec611e3
Lana W <lanawehbeh12@gmail.com>
2023-11-12 10:55:11 -0500

    Untested Controller Established

--- flex/src/main/java/consonants/flex/interface_adapter/get_user_info/GetUserInfoController.java
+++ flex/src/main/java/consonants/flex/interface_adapter/get_user_info/GetUserInfoController.java
@@ -1,4 +1,35 @@
 package consonants.flex.interface_adapter.get_user_info;
 
+import consonants.flex.entity.Claim;
+import consonants.flex.entity.Client;
+import consonants.flex.use_case.get_user_info.GetUserInfoInputBoundary;
+import consonants.flex.use_case.view_all_claims.ViewAllClaimsInputBoundary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@CrossOrigin(origins ="*")
+@RequestMapping("/claims")
 public class GetUserInfoController {
+    @Autowired
+    private GetUserInfoInputBoundary getUserInfoInteractor;
+
+    @GetMapping("/{accountId}")
+
+    public ResponseEntity<Client> getAllClaims(int accountId) {
+        return getUserInfoInteractor.execute(accountId);
+    }
+
+
+
+
+
 }
+
+
+
