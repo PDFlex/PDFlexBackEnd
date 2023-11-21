4163d83a1a698d17b8d0c2865b9a3cc6107541e5
mr. ben <99620362+bensandoval@users.noreply.github.com>
2023-11-21 16:06:46 -0500

    Initial Tests

--- flex/src/test/java/consonants/flex/FlexApplicationTests.java
+++ flex/src/test/java/consonants/flex/FlexApplicationTests.java
@@ -1,13 +1,22 @@
 package consonants.flex;
 
+import consonants.flex.interface_adapter.login.LoginController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FlexApplicationTests {
 
+	@Autowired
+	private LoginController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		System.out.println("hi!");
+		System.out.println(assertThat(controller).isNotNull());
 	}
 
 }
