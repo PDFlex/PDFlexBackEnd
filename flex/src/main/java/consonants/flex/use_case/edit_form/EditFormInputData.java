64ea59c5ad5da7db0a021ce4708a6b15b5ec22be
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2023-11-26 16:28:36 -0500

    Implemented post methods in contoller

--- flex/src/main/java/consonants/flex/use_case/edit_form/EditFormInputData.java
+++ flex/src/main/java/consonants/flex/use_case/edit_form/EditFormInputData.java
@@ -1,4 +1,12 @@
 package consonants.flex.use_case.edit_form;
 
+import java.util.Map;
+
 public class EditFormInputData {
+    Map<String, Object> formInfo; // Why does the variable get greyed out when I use private final?
+
+    // Data manipulation
+    public EditFormInputData(Map<String, Object> formInfo) {
+        this.formInfo = formInfo;
+    }
 }
