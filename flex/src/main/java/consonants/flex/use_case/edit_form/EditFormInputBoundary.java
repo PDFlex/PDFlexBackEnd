ae0351b63e07c7d2ecae38baca588f2218c0d325
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2023-11-25 23:19:43 -0500

    Created empty files, began implementing the controller

---
+++ flex/src/main/java/consonants/flex/use_case/edit_form/EditFormInputBoundary.java
@@ -0,0 +1,5 @@
+package consonants.flex.use_case.edit_form;
+
+public interface EditFormInputBoundary {
+    void execute(EditFormInputData editFormInputData);
+}
---
+++ flex/src/main/java/consonants/flex/use_case/edit_form/EditFormOutputData.java
@@ -0,0 +1,5 @@
+package consonants.flex.use_case.edit_form;
+
+public class EditFormOutputData {
+    // TODO: May delete
+}
