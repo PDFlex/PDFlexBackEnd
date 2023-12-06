8e3be9984122c2580915cfc4b01c52f589de01eb
vithushon <v.thayalan@mail.utoronto.ca>
2023-12-06 18:05:44 -0500

    Added changes such that form status is set to "CONFIRMED" during SaveFormUseCase

--- flex/src/main/java/consonants/flex/use_case/save_form/SaveFormInteractor.java
+++ flex/src/main/java/consonants/flex/use_case/save_form/SaveFormInteractor.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SaveFormInteractor implements SaveFormInputBoundary{
     @Autowired
@@ -17,6 +19,10 @@ public class SaveFormInteractor implements SaveFormInputBoundary{
     @Override
     public ResponseEntity<Object> execute(SaveFormInputData saveFormInputData) {
         saveFormDataAccessObject.modifyForm(saveFormInputData.getClaimId(), saveFormInputData.getFormFields());
+        List<Integer> lst = saveFormDataAccessObject.getClaimFormIds(saveFormInputData.getClaimId());
+        for (int formId : lst) {
+            saveFormDataAccessObject.modifyFormStatus(formId, "CONFIRMED");
+        }
         return new ResponseEntity<>("Form Information: " + saveFormInputData.getFormFields(), HttpStatus.OK);
     }
 }
