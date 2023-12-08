cd6299089476523c8753e9512e6c81119a8a24bf
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2023-12-07 19:48:18 -0500

    deleted filler files and redundant classes/folders

--- flex/src/main/java/consonants/flex/use_case/upload_form/UploadFormOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package consonants.flex.use_case.upload_form;
-
-public interface UploadFormOutputBoundary {
-    // this use case doesn't require any input data
-}
--- flex/src/main/java/consonants/flex/use_case/upload_form/UploadFormOutputData.java
+++
@@ -1,6 +0,0 @@
-package consonants.flex.use_case.upload_form;
-
-public class UploadFormOutputData{
-    // this use case doesn't require any output data
-
-}
